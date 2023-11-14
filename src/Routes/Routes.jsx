5e43ade90b96bca471e9462d063f562c49ccfc7b
AHMEDNABIB <nabibpallab@gmail.com>
2023-11-15 00:17:10 +0600

    project setup complete

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,11 @@
+import Main from "../Layout/Main";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element:<Main></Main>,
+	},
+]);
+
+
+export default router
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import './index.css'
+import router from './Routes/Routes.jsx';
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
