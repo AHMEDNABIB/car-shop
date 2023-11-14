2068866a8417c98f323aff91f5540d826c5e7ce8
AHMEDNABIB <nabibpallab@gmail.com>
2023-11-15 01:33:15 +0600

    banner ain header and footer added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import './index.css'
-import router from './Routes/Routes.jsx';
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import router from "./Routes/Routes.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>
+	<div className="max-w-7xl mx-auto">
+		<React.StrictMode>
+			<RouterProvider router={router} />
+		</React.StrictMode>
+		,
+	</div>
 );
