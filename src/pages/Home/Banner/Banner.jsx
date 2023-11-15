af0e2ef873e853a22666e7eeaee81472317d03be
AHMEDNABIB <nabibpallab@gmail.com>
2023-11-15 12:21:08 +0600

    about section complete

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -11,7 +11,7 @@ const Banner = () => {
 		<div className="carousel w-full h-[600px]">
 			<div id="slide1" className="carousel-item relative w-full">
 				<img src={img1} className="w-full rounded-xl" />
-				<div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)] ">
+				<div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)] ">
 					<div className="text-white pl-12 space-y-7">
 						<h2 className="text-6xl font-bold w-1/2 ">
 							Affordable Price For Car Servicing
