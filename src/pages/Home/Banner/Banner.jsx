937774685a2c0327504b78008dbde05a0d909bb5
AHMEDNABIB <nabibpallab@gmail.com>
2023-11-15 10:53:29 +0600

    banner complete

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -8,9 +8,29 @@ import img6 from '../../../assets/images/banner/6.jpg'
 const Banner = () => {
     
     return (
-		<div className="carousel w-full">
+		<div className="carousel w-full h-[600px]">
 			<div id="slide1" className="carousel-item relative w-full">
-				<img src={img1} className="w-full" />
+				<img src={img1} className="w-full rounded-xl" />
+				<div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)] ">
+					<div className="text-white pl-12 space-y-7">
+						<h2 className="text-6xl font-bold w-1/2 ">
+							Affordable Price For Car Servicing
+						</h2>
+						<p className='w-1/2'>
+							There are many variations of passages of available,
+							but the majority have suffered alteration in some
+							form
+						</p>
+						<div>
+							<button className="btn btn-neutral mr-5 ">
+								Discover More
+							</button>
+							<button className="btn btn-warning">
+								Latest Project
+							</button>
+						</div>
+					</div>
+				</div>
 				<div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
 					<a href="#slide4" className="btn btn-circle mr-5">
 						❮
