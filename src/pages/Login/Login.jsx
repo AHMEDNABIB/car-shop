f62ecbba73be25a8764c099d4e4d7b0957c924a8
AHMEDNABIB <nabibpallab@gmail.com>
2023-11-15 17:48:24 +0600

    login page design and firebase config complete

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,65 @@
+import { Link } from "react-router-dom";
+import img from "../../assets/images/login/login.svg";
+
+const Login = () => {
+    const handleLogin = (event) => {
+        event.preventDefault()
+    }
+	return (
+		<div className="hero min-h-screen bg-base-200">
+			<div className="hero-content flex-col lg:flex-row">
+				<div className="mr-12 w-1/2">
+					<img src={img} alt="" />
+				</div>
+				<div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+					<h1 className="text-3xl font-bold text-center mt-5">
+						Login now!
+					</h1>
+					<form onSubmit={handleLogin} className="card-body">
+						<div className="form-control">
+							<label className="label">
+								<span className="label-text">Email</span>
+							</label>
+							<input
+								type="email"
+                                placeholder="email"
+                                name="email"
+								className="input input-bordered"
+								required
+							/>
+						</div>
+						<div className="form-control">
+							<label className="label">
+								<span className="label-text">Password</span>
+							</label>
+							<input
+                                type="password"
+                                name="password"
+								placeholder="password"
+								className="input input-bordered"
+								required
+							/>
+							<label className="label">
+								<a
+									href="#"
+									className="label-text-alt link link-hover">
+									Forgot password?
+								</a>
+							</label>
+						</div>
+						<div className="form-control mt-6">
+							<input
+								className="btn btn-primary"
+								type="submit"
+								value="Login"
+							/>
+						</div>
+                    </form>
+                    <p className="text-center mb-10">New to Cars Shop ? <Link to="/signup" className="text-orange-600 font-bold">Sign up</Link></p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Login;
