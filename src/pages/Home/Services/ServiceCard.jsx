9abd34e7d90c224646851a90cc5edbf3c7663df8
AHMEDNABIB <nabibpallab@gmail.com>
2023-11-15 16:42:53 +0600

    service section added

---
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const ServiceCard = ({ service }) => {
+    const {title,img,price} =service
+	return (
+		<div className="card w-96 bg-base-100 shadow-xl">
+			<figure className="px-10 pt-10">
+				<img
+					src={img}
+                    alt="service"
+					className="rounded-xl"
+				/>
+			</figure>
+			<div className="card-body ">
+                <h2 className="card-title">{title}</h2>
+                <p className="text-xl text-orange-500">Price: { price }</p>
+				
+			</div>
+		</div>
+	);
+};
+
+export default ServiceCard;
--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import logo from '../../../assets/logo.svg'
 
 const Footer = () => {
 	return (
-		<footer className="footer p-10 bg-base-200 text-base-content">
+		<footer className="footer p-10 bg-base-200 text-base-content mt-[113px]">
 			<aside>
 				
                 <img src={ logo } alt="" />
