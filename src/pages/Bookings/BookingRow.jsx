4de8c72e95cb796c3e63055ce88fbc994519c4ff
AHMEDNABIB <nabibpallab@gmail.com>
2023-11-16 22:31:09 +0600

    confirm booking by using a patch update

--- src/pages/Bookings/BookingRow.jsx
+++ src/pages/Bookings/BookingRow.jsx
@@ -1,25 +1,35 @@
+const BookingRow = ({ booking, handleDelete, handleBookingConfirm }) => {
+	const { _id, customerName, img, email, date, service_id, service, price,status } =
+		booking;
 
-
-const BookingRow = ({ booking }) => {
-    const { _id, customerName, img, email, date, service_id, service ,price} = booking;
-    return (
+	return (
 		<tr>
 			<th>
-				<label>
-					<input type="checkbox" className="checkbox" />
-				</label>
+				<button
+					onClick={() => handleDelete(_id)}
+					className="btn btn-circle btn-sm btn-outline">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						className="h-6 w-6"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor">
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="2"
+							d="M6 18L18 6M6 6l12 12"
+						/>
+					</svg>
+				</button>
 			</th>
 			<td>
 				<div className="flex items-center gap-3">
 					<div className="avatar">
 						<div className=" w-24 h-24 rounded">
-							<img
-								src={img}
-								alt="Service Image"
-							/>
+							<img src={img} alt="Service Image" />
 						</div>
 					</div>
-					
 				</div>
 			</td>
 			<td>
@@ -30,7 +40,15 @@ const BookingRow = ({ booking }) => {
 			<td>{email}</td>
 			<td>${price}</td>
 			<th>
-				<button className="btn btn-ghost btn-xs">details</button>
+				{status === "confirm" ? (
+					<span className="font-bold text-primary">Confirmed</span>
+				) : (
+					<button
+						onClick={() => handleBookingConfirm(_id)}
+						className="btn btn-ghost btn-xs">
+						Please Confirm
+					</button>
+				)}
 			</th>
 		</tr>
 	);
