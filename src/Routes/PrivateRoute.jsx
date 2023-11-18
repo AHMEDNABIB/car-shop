452d7023bea2cb264d56ad104d27bc992078709e
AHMEDNABIB <nabibpallab@gmail.com>
2023-11-18 20:55:54 +0600

    refactor

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,17 +1,18 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../providers/AuthProvider';
-import { Navigate } from 'react-router-dom';
+import React, { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { AuthContext } from "../providers/AuthProvider";
 
-const PrivateRoute = ({children}) => {
-    const { user, loading } = useContext(AuthContext)
-    if (loading) {
-        return <progress className="progress w-56"></progress>;
-    }
+const PrivateRoute = ({ children }) => {
+	const { user, loading } = useContext(AuthContext);
+	const location = useLocation();
+	if (loading) {
+		return <progress className="progress w-56"></progress>;
+	}
 
-    if (user?.email) {
-        return children
-    }
-    return <Navigate to="/login" replace></Navigate>
+	if (user?.email) {
+		return children;
+	}
+	return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
 };
 
 export default PrivateRoute;
