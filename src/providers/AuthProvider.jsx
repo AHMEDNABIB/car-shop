eb3688e940e4a7e19b16e1693f1c7bf38631105c
AHMEDNABIB <nabibpallab@gmail.com>
2023-11-20 14:27:17 +0600

    jwt token for signup and social login user

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,8 +1,10 @@
 import {
+	GoogleAuthProvider,
 	createUserWithEmailAndPassword,
 	getAuth,
 	onAuthStateChanged,
 	signInWithEmailAndPassword,
+	signInWithPopup,
 	signOut,
 } from "firebase/auth";
 import React, { createContext, useEffect, useState } from "react";
@@ -14,6 +16,8 @@ const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
 	const [user, setUser] = useState(null);
 	const [loading, setLoading] = useState(true);
+	const googleProvider = new GoogleAuthProvider();
+
 	const createUser = (email, password) => {
 		setLoading(true);
 		return createUserWithEmailAndPassword(auth, email, password);
@@ -22,6 +26,11 @@ const AuthProvider = ({ children }) => {
 	const signIn = (email, password) => {
 		setLoading(true);
 		return signInWithEmailAndPassword(auth, email, password);
+	};
+
+	const googleSignIn = () => {
+		setLoading(true);
+		return signInWithPopup(auth, googleProvider);
 	};
 
 	const logOut = () => {
@@ -33,6 +42,30 @@ const AuthProvider = ({ children }) => {
 			setUser(currentUser);
 			// console.log("current user", currentUser);
 			setLoading(false);
+
+			 if (currentUser && currentUser.email) {
+					const loggedUser = {
+						email: currentUser.email,
+					};
+					fetch("http://localhost:5000/jwt", {
+						method: "POST",
+						headers: {
+							"content-type": "application/json",
+						},
+						body: JSON.stringify(loggedUser),
+					})
+						.then((res) => res.json())
+						.then((data) => {
+							console.log("jwt response", data);
+							// Warning: Local storage is not the best (second best place) to store access token
+							localStorage.setItem(
+								"car-access-token",
+								data.token
+							);
+						});
+				} else {
+					localStorage.removeItem("car-access-token");
+				}
 		});
 
 		return () => {
@@ -46,6 +79,7 @@ const AuthProvider = ({ children }) => {
 		createUser,
 		signIn,
 		logOut,
+		googleSignIn
 	};
 	return (
 		<AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
